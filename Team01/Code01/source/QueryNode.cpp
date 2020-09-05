32c441a17503297c738273c4824a8d28ca9e9107
limyuhui <yuhuilim1999@gmail.com>
2020-09-06 03:16:53 +0800

    Fix some compilation errors

--- Team01/Code01/source/QueryNode.cpp
+++ Team01/Code01/source/QueryNode.cpp
@@ -1,11 +1,14 @@
 #include "QueryNode.h"
 
+QueryNode::QueryNode(){}
+
+/*
 QUERY_NODE_POINTER QueryNode::createNode(NODE_TYPE node_type, QUERY_NODE_POINTERS children) {
-	QueryNode new_node = QueryNode();
-	new_node.node_type = node_type;
+	node_type = node_type;
 	new_node.children = children;
 	return new_node;
 }
+*/
 
 NODE_TYPE QueryNode::getNodeType() {
 	return node_type;
