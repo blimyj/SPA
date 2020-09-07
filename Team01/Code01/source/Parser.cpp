5d7672f15dbe8c148cc342fe5861f2e973fb0fda
blimyj21@gmail.com <blimyj21@gmail.com>
2020-09-08 04:01:02 +0800

    Update restrictions for ending of statements and opening segments

--- Team01/Code01/source/Parser.cpp
+++ Team01/Code01/source/Parser.cpp
@@ -89,8 +89,27 @@ a=3;
 */
 
 
+/*
+For read, print, assign, call statements, the terminal ';' to indicate the end of the statement.
+
+For procedure, while, if-then, else statement, the terminal '{' indicates the end of the opening segment of the container statement.
+
+For statement lists, the terminal  '}' to indicate the end of the statement list.
+
+Please note that for the above statements that the end of the statement, statement list, or opening segment
+DOES NOT indicate the end of the line. There can be combinations of the aforementioned in a single line. (A 50 line program could be compressed to 1 line)
+*/
+
+/*
+Due to the possibility of read, print, if, else, then, while, procedure, call being variable names,
+we must look ahead at least 1 token to see if it is an assign statement.
+*/
 
 int parseLine(STRING str) {
+	
+	//Taking a string, we want to tokenize 1 at a time, with each token telling us what we expect next.
+	
+	
 	//Takes out only first set of whitespaces
 
 	//check for 'if' / 'while' in the first 2 and 5 chars  respectively & a '(' exists (Otherwise it could be a var name)
@@ -112,55 +131,83 @@ int parseLine(STRING str) {
 }
 
 int parseWhile(STRING str) {
-	//We assume that this line will either terminate with '{' or ';' with the exception of whitespaces and newline?
+	//We assume that this opening segment will either terminate with '{' or ';' with the exception of whitespaces and newline?
 	return 0;
 }
 
-int parseIf(STRING str) {
-	//We assume that this line will either terminate with '{' or ';' with the exception of whitespaces and newline?
+int parseIfThen(STRING str) {
+	//We assume that this opening segment will either terminate with '{' or ';' with the exception of whitespaces and newline?
 
 	return 0;
 }
 
 int parseElse(STRING str) {
 	//We assume that '{' will follow 'else'
-	//We assume that this line will either terminate with '{' or ';' with the exception of whitespaces and newline?
+	//We assume that this opening segment will either terminate with '{' or ';' with the exception of whitespaces and newline?
 	return 0;
 }
 
 int parseAssign(STRING str) {
-	//We assume that this line will terminate with ';'
+	//We assume that this statement will terminate with ';'
 
 	return 0;
 }
 
 int parseRead(STRING str) {
-	//We assume that this line will terminate with ';'
+	//We assume that this statement will terminate with ';'
 	return 0;
 }
 
 int parsePrint(STRING str) {
-	//We assume that this line will terminate with ';'
+	//We assume that this statement will terminate with ';'
 	return 0;
 }
 
 int parseProcedure(STRING str) {
-	//We assume that this line will either terminate with '{' or ';' with the exception of whitespaces and newline?
+	//We assume that this opening segment will either terminate with '{' or ';' with the exception of whitespaces and newline?
 	return 0;
 }
 
 int parseCall(STRING str) {
-	//We assume that this line will terminate with ';'
+	//We assume that this statement will terminate with ';'
 	return 0;
 }
 
 int parseStmtListClose(STRING str) {
-	//We make no assumptions that '}' ends this line. We check for any leftover non-whitespace characters
-	//and call parseLine on the remainding stringif there are. 
-
-	//look up parent to close the stmt node and change the parent node
-	//If closing a stmtList Node, we must ensure there exists at least 1 stmt
+	//Part 1: When closing a stmtList Node, 
+		//we must ensure there exists at least 1 stmt (if == 0 child)
+			//print parsing error -> incorrect code
 	//remove '}'
+	
+
+	//Part 2:
+	//Assuming we maintain curr_parent_node_
+
+	//There are 2 different ways to change the curr_parent_node_ (Refer to StmtListCloseChangeParentNode image)
+	//if (curr_parent_node_.getParent().getType() == IF_NODETYPE_ENUM)
+		//cast to parent of curr_parent_node_ to IF_NODE*
+		//check if its ThenStatementListNode is equal to curr_parent_node_ (after we cast curr_parent_node_ to STMT_LIST_NODE*)
+		//if ( ((IF_NODE*) curr_parent_node_.getParent()).getThenStatementListNode()) == ((STMT_LIST_NODE*) curr_parent_node_) )
+			//change curr_parent_node_ to point to ELSE_STMT_LIST_NODE
+			//We assume that it was pre-created when we created the IF_STMT_LIST_NODE
+			//curr_parent_node_ =  (AST_NODE*) (((IF_NODE*) curr_parent_node_.getParent()).getElseStatementListNode())
+		//else
+			//change curr_parent_node_ to point to STMT_LIST_NODE that contains the IF_NODE
+			//curr_parent_node_ = curr_parent_node_.getParent().getParent()
+	//else
+		//change curr_parent_node_ to point to grandparent node of curr_parent_node_
+		//curr_parent_node_ must be STMT_LIST_NODE
+		//it's parent can be PROCEDURE_NODE, WHILE_NODE or IF_NODE (if this is an ELSE_STMT_LIST_NODE)
+		//therefore grandparent node can be PROGRAM_NODE or STMT_LIST_NODE
+		//curr_parent_node_ = curr_parent_node_.getParent().getParent()
+
+
+
+
+
+	//Part 3:
+	//We make no assumptions that '}' ends this line. We check for any leftover non-whitespace characters
+	//and call parseLine on the remainding string if there are remaining chars. 
 	//check if there is remaining str
 		//call parseLine on remainder of str
 	//else end
