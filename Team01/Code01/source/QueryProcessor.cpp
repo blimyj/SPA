177f5ade5a588f3450f3786db1ecb30bf7269f62
limyuhui <yuhuilim1999@gmail.com>
2020-09-08 02:09:15 +0800

    Implement QueryProcessor structure

---
+++ Team01/Code01/source/QueryProcessor.cpp
@@ -0,0 +1,21 @@
+#include "QueryProcessor.h"
+//#include "QueryEvaluator.h"
+
+QueryProcessor::QueryProcessor(){}
+
+QUERY_RESULT QueryProcessor::processQuery(QUERY query) {
+	/*
+	* QueryPreProcessor pre_processor = QueryPreProcessor();
+	* PROCESSED_QUERY process_queries = pre_processor.parse(QUERY q)
+	* std::string 
+	* PROCESSED_SYNONYMS processed_synonyms = pre_processor.preProcessSynonyms(QUERY q); //returns table of synonym nodes
+	* PROCESSED_CLAUSES processed_clauses = pre_processor.preProcessClauses(QUERY q); //returns query tree of clause nodes
+	*/
+
+	/*
+	* QueryEvaluator query_evaluator = QueryEvaluator();
+	* QUERY_RESULT query_result = query_evaluator.evaluateQuery(processed_synonyms, processed_clauses);
+	* return query_result;
+	*/
+	return "result as string";
+}
