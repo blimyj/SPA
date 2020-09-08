f33db70500c15d2da3d08af63e0a36b3ce21fa7f
Kyzure <Kyzurene@gmail.com>
2020-09-08 16:25:15 +0800

    Push to branch first

--- Team01/Code01/source/PKB/Relationships/FollowsTable.cpp
+++ Team01/Code01/source/PKB/Relationships/FollowsTable.cpp
@@ -5,7 +5,7 @@
 typedef void VOID;
 typedef bool BOOLEAN;
 typedef std::string STATEMENT_NUMBER;
-typedef std::unordered_map<STATEMENT_NUMBER, STATEMENT_NUMBER> TABLE;
+typedef std::unordered_map<STATEMENT_NUMBER, STATEMENT_NUMBER>* TABLE;
 
 VOID FollowsTable::addFollows(STATEMENT_NUMBER s1, STATEMENT_NUMBER s2) {
     table_.insert({ s1, s2 });
