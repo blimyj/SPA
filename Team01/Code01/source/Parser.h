59ab54850a4359da340ca70eef68ad50f49fc114
blimyj21@gmail.com <blimyj21@gmail.com>
2020-09-10 19:10:22 +0800

    Add tokenization to parser

--- Team01/Code01/source/Parser.h
+++ Team01/Code01/source/Parser.h
@@ -13,4 +13,12 @@ typedef std::vector<std::string> LIST_OF_STRINGS;
 int Parse();
 int parseFile(STRING);
 int parseText(LIST_OF_STRINGS);
+<<<<<<< HEAD
 >>>>>>> Change Parser assumptions
+=======
+
+int parsePrint(STRING str, int ind);
+int parseRead(STRING str, int ind);
+
+STRING getNextToken(std::istreambuf_iterator<char>* iter, std::istreambuf_iterator<char> eos);
+>>>>>>> Add tokenization to parser
