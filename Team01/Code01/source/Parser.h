267ba18ab12644f5cc68abe66ce2964a534fbf95
blimyj21@gmail.com <blimyj21@gmail.com>
2020-09-10 19:29:40 +0800

    TEMP WORK: Prior to importing AST_API

--- Team01/Code01/source/Parser.h
+++ Team01/Code01/source/Parser.h
@@ -6,9 +6,13 @@ int Parse();
 
 #include <vector>
 #include <string>
+#include <deque>
 
 typedef std::string STRING;
 typedef std::vector<std::string> LIST_OF_STRINGS;
+typedef std::deque<STRING>* STMT_TOKEN_QUEUE;
+typedef std::deque<STRING>* PROCESS_TOKEN_QUEUE;
+
 
 int Parse();
 int parseFile(STRING);
@@ -17,8 +21,9 @@ int parseText(LIST_OF_STRINGS);
 >>>>>>> Change Parser assumptions
 =======
 
-int parsePrint(STRING str, int ind);
-int parseRead(STRING str, int ind);
+int parseRead(STMT_TOKEN_QUEUE stmt_tok_queue, PROCESS_TOKEN_QUEUE proc_tok_queue);
+int parsePrint(STMT_TOKEN_QUEUE stmt_tok_queue, PROCESS_TOKEN_QUEUE proc_tok_queue);
+int parseAssign(STMT_TOKEN_QUEUE stmt_tok_queue, PROCESS_TOKEN_QUEUE proc_tok_queue);
 
 STRING getNextToken(std::istreambuf_iterator<char>* iter, std::istreambuf_iterator<char> eos);
 >>>>>>> Add tokenization to parser
