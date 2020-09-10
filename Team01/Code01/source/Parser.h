bda07aa321dcea19b92c86195f880d5f6b6504db
blimyj21@gmail.com <blimyj21@gmail.com>
2020-09-10 23:55:45 +0800

    Fix rebase conflicts

--- Team01/Code01/source/Parser.h
+++ Team01/Code01/source/Parser.h
@@ -1,8 +1,4 @@
 #pragma once
-<<<<<<< HEAD
-using namespace std;
-int Parse();
-=======
 
 #include <vector>
 #include <string>
@@ -17,13 +13,10 @@ typedef std::deque<STRING>* PROCESS_TOKEN_QUEUE;
 int Parse();
 int parseFile(STRING);
 int parseText(LIST_OF_STRINGS);
-<<<<<<< HEAD
->>>>>>> Change Parser assumptions
-=======
 
+int parseProcedure(STMT_TOKEN_QUEUE stmt_tok_queue, PROCESS_TOKEN_QUEUE proc_tok_queue);
 int parseRead(STMT_TOKEN_QUEUE stmt_tok_queue, PROCESS_TOKEN_QUEUE proc_tok_queue);
 int parsePrint(STMT_TOKEN_QUEUE stmt_tok_queue, PROCESS_TOKEN_QUEUE proc_tok_queue);
 int parseAssign(STMT_TOKEN_QUEUE stmt_tok_queue, PROCESS_TOKEN_QUEUE proc_tok_queue);
 
 STRING getNextToken(std::istreambuf_iterator<char>* iter, std::istreambuf_iterator<char> eos);
->>>>>>> Add tokenization to parser
