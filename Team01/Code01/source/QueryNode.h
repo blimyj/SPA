829be0f1a26a877c9fa779dded243b147b3dcc71
limyuhui <yuhuilim1999@gmail.com>
2020-09-11 03:27:35 +0800

    Use enum class for QueryNodeType

--- Team01/Code01/source/QueryNode.h
+++ Team01/Code01/source/QueryNode.h
@@ -1,24 +1,26 @@
 #pragma once
 
+#include "QueryNodeType.h"
+
 #include <string>
 
 class QueryNode;
 
-typedef std::string NODE_TYPE;
+typedef QueryNodeType NODE_TYPE;
 typedef QueryNode* QUERY_NODE_POINTERS;
-typedef std::string SYNONYM;
+typedef std::string SYNONYM_NAME;
 
 class QueryNode {
 private:
 	NODE_TYPE node_type;
 	QUERY_NODE_POINTERS children = NULL; //array of pointers to children nodes
-	SYNONYM synonym;
+	SYNONYM_NAME synonym;
 
 public: 
 	void setNodeType(NODE_TYPE node_type);
-	void setSynonymNode(NODE_TYPE node_type, SYNONYM synonym); //synonym nodes MUST have snyonym name
+	void setSynonymNode(NODE_TYPE node_type, SYNONYM_NAME synonym); //synonym nodes MUST have snyonym name
 	NODE_TYPE getNodeType();
 	void setChildren(QUERY_NODE_POINTERS children); //need to add all children at once, in the right order
 	QUERY_NODE_POINTERS getChildren();
-	SYNONYM getSynonym();
+	SYNONYM_NAME getSynonym();
 };
