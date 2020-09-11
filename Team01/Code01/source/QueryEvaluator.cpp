05c2228c432b666aec9cadc8ae8a2b7dbf31d86d
limyuhui <yuhuilim1999@gmail.com>
2020-09-11 10:49:10 +0800

    Add PKB stub

--- Team01/Code01/source/QueryEvaluator.cpp
+++ Team01/Code01/source/QueryEvaluator.cpp
@@ -3,10 +3,17 @@
 #include <string>
 #include <vector>
 
-
 QUERY_RESULT QueryEvaluator::evaluateQuery(PROCESSED_SYNONYMS synonyms, PROCESSED_CLAUSES clauses) {
 	std::vector<std::string> result_list;
-	result_list.push_back("yo yo im here");
+	result_list.push_back("yo yo im here"); //initialise to empty_result
+	QueryNode chosen_return_type;
+	
+	QUERY_NODE_POINTERS children = clauses.getChildren();
+	if (clauses.getNodeType() == QueryNodeType::select) {
+		result_list[0] = result_list[0] + "\n" + "Select Worked";
+		chosen_return_type = children[0];
+		result_list[0] = result_list[0] + "\n" + "chosen return synonym name: " + chosen_return_type.getSynonymName();
+	}
 	
 	//intermediate result_list can have many synonyms
 	//but final result should return just 1 string/empty
--- Team01/Code01/source/QueryProcessor.h
+++ Team01/Code01/source/QueryProcessor.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "PKB.h"
+
 #include <string>
 
 class QueryProcessor;
