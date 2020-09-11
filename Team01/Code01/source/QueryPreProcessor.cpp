c376eabdfc560bde353e04bf435e063f86ab1f5f
hmtey <huiming.tey@gmail.com>
2020-09-11 13:41:18 +0800

    Updated Query Pre-Processor header file

--- Team01/Code01/source/QueryPreProcessor.cpp
+++ Team01/Code01/source/QueryPreProcessor.cpp
@@ -1,52 +1,144 @@
 #include <regex>
 #include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 
 #include "QueryPreProcessor.h"
+#include "QueryNodeType.h"
 
-PROCESSED_SYNONYMS preProcessSynonyms(QUERY q) {
-	// find the synonyms in the query, which come before semicolons
 
-	// validate grammar of synonyms
+const std::regex name_format_("[a-zA-Z][a-zA-Z0-9]*");
+const std::regex declaration_format_("\\s*(stmt|read|print|while|if|assign|variable|constant|procedure)\\s+[a-zA-Z][a-zA-Z0-9]*\\s*(\\,\\s*[a-zA-Z][a-zA-Z0-9]*)*\\s*\\;\\s*");
+//const std::regex clause_format_("Select\\s+[a-zA-Z][a-zA-Z0-9]*(()?()?|()?()?)");
+
+SPLIT_QUERY splitQuery(QUERY q) {
+	SPLIT_QUERY split_q;
+	DECLARATIONS d = "";
+	CLAUSES c = "";
+
+	if (isValidStructure(q)) {
+		// split the query into declarations and clauses
+		std::string delimiter = ";";
+		int split_index = q.rfind(delimiter);
+		d = q.substr(0, split_index));
+		c = q.substr(split_index + 1);
+	}
+	split_q.push_back(d);
+	split_q.push_back(c);
+
+	return split_q;
+}
+
+PROCESSED_SYNONYMS preProcessSynonyms(DECLARATIONS d) {
+	PROCESSED_SYNONYMS proc_s;
+	VALIDATION_RESULT is_valid;
+
+	// process declaration-by-declaration
+	std::string delimiter = ";";
+	int index = 0;
+	int split_index = s.find(delimiter);
+
+	if (split_index == -1) {
+		// if there is only one declaration (no other semicolons)
+		is_valid = isValidDeclaration(d);
+
+		// break into synonyms
+		// create query node for each synonym
+		// add query node to PROCESSED_SYNONYMS
+	}
+	else {
+		while (split_index != -1) {
+
+		}
+	}
+
+	if (is_valid) {
+		return proc_s;
+	}
+	else {
+		return PROCESSED_SYNONYMS empty_map;
+	}
 
 }
 
-PROCESSED_CLAUSES preProcessClauses(QUERY q) {
-	// find the clauses in the query, which come after semicolons
+PROCESSED_CLAUSES preProcessClauses(PROCESSED_SYNONYMS proc_s, CLAUSES c) {
+
+	// find the clauses in the query, which come after the last semicolon
+
 
 	// validate grammar of clauses
 
 }
 
-VALIDATION_RESULT validateSynonyms(SYNONYMS s) {
-	VALIDATION_RESULT result = true;
-
-	// check if any synonyms are declared
-
-	// check if declaration follows the rule of: design-entity synonym (‘,’ synonym)* ‘;’
-
-	// check if name follows rule of: LETTER ( LETTER | DIGIT )*
-
-	return result;
+VALIDATION_RESULT isValidStructure(QUERY q) {
+	/*
+	Validation rules:
+		- Query is not empty
+		- Has a declaration
+		- Has 'Select'
+	*/
+	if (q.length() <= 0) {
+		return false;
+	}
+	else if (q.find(";") == -1) {
+		return false;
+	}
+	else if (q.find("Select") == -1) {
+		return false;
+	}
+	else {
+		return true;
+	}
 }
 
-VALIDATION_RESULT validateClauses(CLAUSES c) {
-	VALIDATION_RESULT result = true;
-
-	// check if clauses start with "Select"
-
-	// check if has at most one such that and pattern clauses
-
-	// check if clause has correct number of arguments
-
-	return result;
+VALIDATION_RESULT isValidDeclaration(SINGLE_DECLARATION single_d) {
+	/*
+	Validation rules:
+		- Declaration must not be empty
+		- Follows the format: design-entity synonym (‘,’ synonym)* ‘;’
+		- Declared design entiry is valid
+		- Synonym names follows the format: LETTER ( LETTER | DIGIT )*
+	*/
+	if (single_d.length() <= 0) {
+		return false;
+	}
+	else if (std::regex_match(single_d, declaration_format_)) {
+		return false;
+	}
+	else {
+		return true;
+	}
 }
 
-VALIDATION_RESULT validateQuery(PROCESSED_SYNONYMS s, PROCESSED_CLAUSES c) {
-	VALIDATION_RESULT result = true;
+VALIDATION_RESULT isValidClause(CLAUSES c) {
+	/*
+	Validation rules:
+		- There is a synonym after 'Select'
+		- Has at most one 'such that' and 'pattern' clauses
+		- Clauses have correct number of arguments
+	*/
+	if () {
+	}
+
+}
+
+VALIDATION_RESULT isValidQuery(PROCESSED_SYNONYMS proc_s, PROCESSED_CLAUSES proc_c) {
 
 	// check if arguments used in clauses are declared
 
+	// check if type of arguments used for clauses are correct
+
+}
+
+VALIDATION_RESULT isValidRelation(PROCESSED_SYNONYMS proc_s, PROCESSED_CLAUSE proc_c) {
+
 	// check if type of arguments used for clause is correct
 
-	return result;
+}
+
+VALIDATION_RESULT isValidPattern(PROCESSED_SYNONYMS proc_s, PROCESSED_CLAUSE proc_c) {
+
+	// check if type of arguments used for clause is correct
+
 }
--- Team01/Code01/source/QueryPreProcessor.h
+++ Team01/Code01/source/QueryPreProcessor.h
@@ -1,48 +1,79 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 #include "QueryNode.h"
 
-typedef string QUERY;
-typedef QueryNode PROCESSED_SYNONYMS;
+typedef std::string QUERY;
+typedef std::vector<std::string> SPLIT_QUERY;
+typedef std::string SINGLE_DECLARATION;
+typedef std::string DECLARATIONS;
+typedef std::unordered_map<std::string, QueryNode> PROCESSED_SYNONYMS;
+typedef std::string CLAUSES;
 typedef QueryNode PROCESSED_CLAUSES;
+typedef QueryNode PROCESSED_CLAUSE;
 typedef bool VALIDATION_RESULT;
-typedef string SYNONYMS;
-typedef string CLAUSES;
 
 class QueryPreProcessor {
 	/* Overview: Pre-processes queries into a suitable data structure for the evaluator */
 
 public:
-	PROCESSED_SYNONYMS preProcessSynonyms(QUERY q);
+	SPLIT_QUERY splitQuery(QUERY q);
 	/*
 		Description:
-		  Normal: Returns a PROCESSED_SYNONYMS
+		  Normal: Returns a SPLIT_QUERY
 		*/
 
-	PROCESSED_CLAUSES preProcessClauses(QUERY q);
+	PROCESSED_SYNONYMS preProcessSynonyms(DECLARATIONS d);
 	/*
 		Description:
-		  Normal: Returns a PROCESSED_CLAUSES
+		  Normal: Returns PROCESSED_SYNONYMS
+		  Abnormal: If synonyms are not valid, returns an empty map
 		*/
 
-	VALIDATION_RESULT validateSynonyms(SYNONYMS s);
+	PROCESSED_CLAUSES preProcessClauses(PROCESSED_SYNONYMS proc_s, CLAUSES c);
 	/*
 		Description:
-		  Normal: Returns a VALIDATION_RESULT based on grammer for synonyms
+		  Normal: Returns PROCESSED_CLAUSES
+		  Abnormal: If clauses are not valid, returns a query node with a null NODE_TYPE
 		*/
 
-	VALIDATION_RESULT validateClauses(CLAUSES c);
+private:
+	VALIDATION_RESULT isValidStructure(QUERY q);
+	/*
+		Description:
+		  Normal: Returns a VALIDATION_RESULT based on general structure of input query
+		*/
+
+	VALIDATION_RESULT isValidDeclaration(SINGLE_DECLARATION single_d);
+	/*
+		Description:
+		  Normal: Returns a VALIDATION_RESULT based on grammer for declarations
+		*/
+
+	VALIDATION_RESULT isValidClause(CLAUSES c);
 	/*
 		Description:
 		  Normal: Returns a VALIDATION_RESULT based on grammar for clauses
 		*/
 
-	VALIDATION_RESULT validateQuery(PROCESSED_SYNONYMS s, PROCESSED_CLAUSES c);
+	VALIDATION_RESULT isValidQuery(PROCESSED_SYNONYMS proc_s, PROCESSED_CLAUSES proc_c);
 	/*
 		Description:
 		  Normal: Returns VALIDATION_RESULT based on looking at the query as a whole
 		*/
 
+	VALIDATION_RESULT isValidRelation(PROCESSED_SYNONYMS proc_s, PROCESSED_CLAUSE proc_c);
+	/*
+		Description:
+		  Normal: Returns a VALIDATION_RESULT based on relationship semantics
+		*/
+
+	VALIDATION_RESULT isValidPattern(PROCESSED_SYNONYMS proc_s, PROCESSED_CLAUSE proc_c);
+	/*
+		Description:
+		  Normal: Returns a VALIDATION_RESULT based on pattern semantics
+		*/
 };
