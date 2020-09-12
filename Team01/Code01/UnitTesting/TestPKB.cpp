18ad23a1bca5cce712493a6ba700092b9f17a0eb
Kyzure <Kyzurene@gmail.com>
2020-09-12 18:06:38 +0800

    Added a unit test to test if it works

--- Team01/Code01/UnitTesting/TestPKB.cpp
+++ Team01/Code01/UnitTesting/TestPKB.cpp
@@ -1,3 +1,5 @@
+#include <typeinfo>
+
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include "PKB.h"
@@ -8,10 +10,17 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 namespace UnitTesting {
 	
 	TEST_CLASS(PKB) {
-	
 	public:
-		TEST_METHOD(getStatements) {
-			PKBBuilder pkb_builder();
+		TEST_METHOD(getAssigns) {
+			PKBBuilder pkb_builder;
+			VAR_NODE_PTR assign_var = std::make_shared<VariableNode>("assigned");
+			VAR_NODE_PTR left_var = std::make_shared<VariableNode>("left");
+			VAR_NODE_PTR right_var = std::make_shared<VariableNode>("right");
+			EXPR_NODE_PTR expr = std::make_shared<ExpressionNode>(ExpressionTypeEnum::plus, left_var, right_var);
+			ASSIGN_NODE_PTR assign = std::make_shared<AssignNode>(assign_var, expr);
+			pkb_builder.addStatementNode(assign);
+			ASSIGN_NODE_PTR_LIST list = pkb_builder.build().getAssigns();
+			Assert::IsTrue(typeid(*assign) == typeid(*list.at(0)));
 		}
 
 	};
