8c6f0b0d4edb9b9a19c5717dbfbb5c4fa1f3b0b1
WeomuCat <weomu.cat@protonmail.com>
2020-09-12 18:59:10 +0800

    Fixing relationship tables :)
    - Cleaning up other tables too

--- Team01/Code01/source/PKB/Relationships/FollowsTable.cpp
+++ Team01/Code01/source/PKB/Relationships/FollowsTable.cpp
@@ -1,13 +1,58 @@
 #include "FollowsTable.h"
 
 VOID FollowsTable::addFollows(STMT_NUM s1, STMT_NUM s2) {
-    table_.insert({ s1, s2 });
+    statement_follows_[s1] = s2;
+    follows_.insert({ s1, s2 });
+    follows_transitive_.insert({ s1, s2 });
 }
 
 BOOLEAN FollowsTable::isFollows(STMT_NUM s1, STMT_NUM s2) {
-    auto it = table_.find(s1);
-    if (it == table_.end()) {
-        return false;
+    // Check if Follows(s1, s2) exists
+    auto it = follows_.find({ s1, s2 });
+    return it != follows_.end();
+}
+
+BOOLEAN FollowsTable::isFollowsTransitive(STMT_NUM s1, STMT_NUM s2) {
+    // Check if Follows*(s1, s2) exists
+    auto it = follows_transitive_.find({ s1, s2 });
+    return it != follows_.end();
+
+    // Memoize transitive relationships
+    std::vector<STMT_NUM> statements;
+    statements.push_back(s1);
+    STMT_NUM curr = s1;
+    while (curr == s2) {
+        auto it = statement_follows_.find(curr);
+        if (it == statement_follows_.end()) {
+            return false;
+        }
+        STMT_NUM next = it->second;
+        for (STMT_NUM s : statements) {
+            follows_transitive_.insert({ s, next });
+        }
+        statements.push_back(next);
+        curr = next;
     }
-    return it->second == s2;
+    return true;
+}
+
+VOID FollowsTable::calculateTransitive() {
+    // Memoize transitive relationships
+    for (std::pair<STMT_NUM, STMT_NUM> f : follows_) {
+        std::vector<STMT_NUM> statements;
+        statements.push_back(f.first);
+        STMT_NUM curr = f.first;
+        while (curr == f.second) {
+            auto it = statement_follows_.find(curr);
+            if (it == statement_follows_.end()) {
+                break;
+            }
+            STMT_NUM next = it->second;
+            for (STMT_NUM s : statements) {
+                follows_transitive_.insert({ s, next });
+            }
+            statements.push_back(next);
+            curr = next;
+        }
+    }
 }
--- Team01/Code01/source/PKB/Relationships/FollowsTable.h
+++ Team01/Code01/source/PKB/Relationships/FollowsTable.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <string>
 #include <unordered_map>
+#include <unordered_set>
 
 typedef void VOID;
 typedef bool BOOLEAN;
@@ -11,10 +12,17 @@ typedef int STMT_NUM;
 class FollowsTable {
 /* Overview: This is a data structure*/
 
-public:
+struct HASH {
+    template <class T1, class T2>
+    std::size_t operator() (const std::pair<T1, T2> &pair) const {
+        return std::hash<T1>()(pair.first) ^ std::hash<T2>()(pair.second);
+    }
+};
+
+private:
     VOID addFollows(STMT_NUM s1, STMT_NUM s2);
         /*
-        Description: Adds to a collection that maps s1 to s2.
+        Description: Stores the relationship Follows(s1, s2) in the table.
         */
 
     BOOLEAN isFollows(STMT_NUM s1, STMT_NUM s2);
@@ -22,7 +30,20 @@ public:
         Description: Returns a BOOLEAN indicating whether or not Follows(s1, s2) holds.
         */
 
-private:
-    std::unordered_map<STMT_NUM, STMT_NUM> table_;
+    BOOLEAN isFollowsTransitive(STMT_NUM s1, STMT_NUM s2);
+        /*
+        Description: Returns a BOOLEAN indicating whether or not Follows*(s1, s2) holds.
+        */
+
+    VOID calculateTransitive();
+        /*
+        Description: Calculates transitive relationships in the table.
+        */
+
+    friend class PKB;
+
+    std::unordered_map<STMT_NUM, STMT_NUM> statement_follows_;
+    std::unordered_set<std::pair<STMT_NUM, STMT_NUM>, HASH> follows_;
+    std::unordered_set<std::pair<STMT_NUM, STMT_NUM>, HASH> follows_transitive_;
 
 };
