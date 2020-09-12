12d3f890792ae266cd0387287e89e8cb852e2867
Kyzure <Kyzurene@gmail.com>
2020-09-12 15:16:56 +0800

    Changed relationship tables to store the values instead of pointers

--- Team01/Code01/source/PKB/Relationships/ParentTable.cpp
+++ Team01/Code01/source/PKB/Relationships/ParentTable.cpp
@@ -1,10 +1,10 @@
 #include "ParentTable.h"
 
-VOID ParentTable::addParent(STMT_NUM_PTR s1, STMT_NUM_PTR s2) {
+VOID ParentTable::addParent(STMT_NUM s1, STMT_NUM s2) {
     table_.insert({ s1, s2 });
 }
 
-BOOLEAN ParentTable::isParent(STMT_NUM_PTR s1, STMT_NUM_PTR s2) {
+BOOLEAN ParentTable::isParent(STMT_NUM s1, STMT_NUM s2) {
     auto it = table_.find(s1);
     if (it == table_.end()) {
         return false;
--- Team01/Code01/source/PKB/Relationships/ParentTable.h
+++ Team01/Code01/source/PKB/Relationships/ParentTable.h
@@ -6,23 +6,23 @@
 
 typedef void VOID;
 typedef bool BOOLEAN;
-typedef std::shared_ptr<int> STMT_NUM_PTR;
+typedef int STMT_NUM;
 
 class ParentTable {
 /* Overview: This is a data structure*/
 
 public:
-    VOID addParent(STMT_NUM_PTR s1, STMT_NUM_PTR s2);
+    VOID addParent(STMT_NUM s1, STMT_NUM s2);
         /*
         Description: Adds to a collection that maps s1 to s2.
         */
 
-    BOOLEAN isParent(STMT_NUM_PTR s1, STMT_NUM_PTR s2);
+    BOOLEAN isParent(STMT_NUM s1, STMT_NUM s2);
         /*
         Description: Returns a BOOLEAN indicating whether or not Parent(s1, s2) holds.
         */
 
 private:
-    std::unordered_map<STMT_NUM_PTR, STMT_NUM_PTR> table_;
+    std::unordered_map<STMT_NUM, STMT_NUM> table_;
 
 };
