1304f3f82309517ba38b098b567a5ccac53c5784
Kyzure <Kyzurene@gmail.com>
2020-09-12 22:45:57 +0800

    Finished documenting API properly

--- Team01/Code01/source/PKB/ASTNode/RelationNode.h
+++ Team01/Code01/source/PKB/ASTNode/RelationNode.h
@@ -16,38 +16,37 @@ class RelationNode : public ASTNode {
 public:
 	RelationNode(RELATION_TYPE relation_type, AST_NODE_PTR left_node_ptr, AST_NODE_PTR right_node_ptr);
 		/*
-		Description: Constructs an RelationNode consisting of a RELATION_TYPE, left and right AST_NODE_PTR.
+		Description: Constructs a RelationNode with the RELATION_TYPE, including the left child AST_NODE_PTR and right child AST_NODE_PTR of this Relation.
 		*/
 
 	BOOLEAN setRelationType(RELATION_TYPE relation_type);
 		/*
-		Description: Returns the RELATION_TYPE enum of the relation.
-		Can be RELATION_TYPE enum of EQ, NEQ, GT, GTE, LT, LTE
+		Description: Returns True if able to set the RELATION_TYPE of this relation, false otherwise.
 		*/
 
 	BOOLEAN setLeftAstNode(AST_NODE_PTR left_node_ptr);
 		/*
-		Description: Returns True if able to set left_node_ as left_node_ptr, false otherwise.
+		Description: Returns True if able to set AST_NODE_PTR to be the left-hand side child of this Relation, false otherwise
 		*/
 
 	BOOLEAN setRightAstNode(AST_NODE_PTR right_node_ptr);
 		/*
-		Description: Returns True if able to set right_node_ as right_node_ptr, false otherwise.
+		Description: Returns True if able to set AST_NODE_PTR to be the right-hand side child of this Relation, false otherwise.
 		*/
 
 	RELATION_TYPE getRelationType();
 		/*
-		Description: Returns the RELATION_TYPE of this relation.
+		Description: Returns the RELATION_TYPE of this ExpressionNode
 		*/
 
 	AST_NODE_PTR getLeftAstNode();
 		/*
-		Description: Returns the AST_NODE_PTR of the left hand side of the expression.
+		Description: Returns the left-hand side child AST_NODE_PTR of this Relation.
 		*/
 
 	AST_NODE_PTR getRightAstNode();
 		/*
-		Description: Returns the AST_NODE_PTR of the right hand side of the expression, throws exception if this is RELATION_TYPE NONE.
+		Description: Returns the right-hand side child AST_NODE_PTR of this Relation.
 		*/
 
 private:
