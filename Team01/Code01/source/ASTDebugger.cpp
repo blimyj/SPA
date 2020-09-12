930ec98e8552915b650d3f976566c37cf1f0dbeb
Janice Ong Yi Wen <55853397+J-Dan23@users.noreply.github.com>
2020-09-12 22:59:41 +0800

    Update ASTDebugger source and header files.

--- Team01/Code01/source/ASTDebugger.cpp
+++ Team01/Code01/source/ASTDebugger.cpp
@@ -2,22 +2,35 @@
 #include <queue>
 #include <vector>
 
-#include "PKB\ASTNode\ASTNode.h"
 #include "ASTDebugger.h"
+#include "PKB/ASTNode/AssignNode.h"
+#include "PKB/ASTNode/ConditionNode.h"
+#include "PKB/ASTNode/ConstantNode.h"
+#include "PKB/ASTNode/ExpressionNode.h"
+#include "PKB/ASTNode/IfNode.h"
+#include "PKB/ASTNode/PrintNode.h"
+#include "PKB/ASTNode/ProcedureNode.h"
+#include "PKB/ASTNode/ProgramNode.h"
+#include "PKB/ASTNode/ReadNode.h"
+#include "PKB/ASTNode/RelationNode.h"
+#include "PKB/ASTNode/StatementListNode.h"
+#include "PKB/ASTNode/StatementNode.h"
+#include "PKB/ASTNode/VariableNode.h"
+#include "PKB/ASTNode/WhileNode.h"
 
 int printTree(AST_NODE_PTR parent_node_ptr) {
     node_queue(parent_node_ptr);
 
     while (node_queue.notEmpty()) {
         AST_NODE_PTR node_ptr = node_queue.dequeue();
-        NODE_TYPE nodeType = getNodeType(node_ptr);
+        NODE_TYPE node_type = getNodeType(node_ptr);
         AST_NODE_PTR_LIST children_nodes = node.getChildren();
-        
-        // Get properties of current node, and print:
-        STRING_LIST node_properties = getProperties(node_ptr);
 
-        for (std::string properties : node_properties) {
-          std::cout << properties << ' ';
+        // Get properties of current node, and print:
+        STRING_LIST node_properties = getProperties(node_ptr, node_type);
+        
+        for (STRING properties : node_properties) {
+            std::cout << properties << '\n';
         }
         
         // Enqueue currrent node's children nodes:
@@ -26,13 +39,82 @@ int printTree(AST_NODE_PTR parent_node_ptr) {
         }
 
         // Print number of children nodes:
-        std::cout << children_nodes.size;
+        std::cout << children_nodes.size << '\n';
     }
 
     return 0;
 }
 
-STRING_LIST getProperties(AST_NODE_PTR node_ptr) {
+STRING_LIST getProperties(AST_NODE_PTR node_ptr, NODE_TYPE node_type, STRINGLIST node_properties) {
+    switch (node_type) {
 
+        case NODE_TYPE::assignNode:
+            node_properties.insert("assignNode");
+            break;
 
+        case NODE_TYPE::conditionNode:
+            node_properties.insert("conditionNode");
+            node_properties.insert(node_ptr.getConditionType());
+            break;
+
+        case NODE_TYPE::constantNode:
+            node_properties.insert("constantNode");
+            node_properties.insert(node_ptr.getValue());
+            break;
+
+        case NODE_TYPE::expressionNode:
+            node_properties.insert("expressionNode");
+            node_properties.insert(node_ptr.getExpression());
+            break;
+
+        case NODE_TYPE::ifNode:
+            node_properties.insert("ifNode");
+            break;
+
+        case NODE_TYPE::printNode:
+            node_properties.insert("printNode");
+            break;
+
+        case NODE_TYPE::procedureNode:
+            node_properties.insert("procedureNode");
+            node_properties.insert(node_ptr.getProcedureName());
+            break;
+
+        case NODE_TYPE::programNode:
+            node_properties.insert("programNode");
+            break;
+
+        case NODE_TYPE::readNode:
+            node_properties.insert("readNode");
+            break;
+
+        case NODE_TYPE::relationNode:
+            node_properties.insert("relationNode");
+            node_properties.insert(node_ptr.getRelationType());
+            break;
+
+        case NODE_TYPE::statementListNode:
+            node_properties.insert("statementListNode");
+            break;
+
+        case NODE_TYPE::statementNode:
+            node_properties.insert("statementNode");
+            node_properties.insert(node_ptr.getStatementNumber());
+            break;
+
+        case NODE_TYPE::variableNode:
+            node_properties.insert("variableNode");
+            node_properties.insert(node_ptr.getVariableName());
+            break;
+
+        case NODE_TYPE::whileNode:
+            node_properties.insert("whileNode");
+            break;
+
+        default:
+            node_properties.insert("Error: No such node.");
+    }
+
+    return node_properties;
+    }
 }
--- Team01/Code01/source/ASTDebugger.h
+++ Team01/Code01/source/ASTDebugger.h
@@ -3,12 +3,12 @@
 #include <queue>
 #include <vector>
 
-#include "PKB\ASTNode\ASTNode.h"
+#include "PKB/ASTNode/ASTNode.h"
 
-typedef std::vector<std::string> STRING_LIST;
+typedef std::string STRING;
+typedef std::vector<STRING> STRING_LIST;
 typedef std::shared_ptr<ASTNode> AST_NODE_PTR;
 typedef std::vector<std::shared_ptr<ASTNode>> AST_NODE_PTR_LIST;
-
 
 class ASTDebugger {
 /* Overview: A root node from an AST made by the Parser is provided to the ASTDebugger,
@@ -16,10 +16,16 @@ which then prints out all nodes in the tree and their properties. */
 
 public:
 	int printTree(AST_NODE_PTR);
+		/*
+		Description: Prints the nodes in the AST out, given the root node.
+		*/
 
 private:
 	AST_NODE_PTR_LIST children_nodes;
 	std::queue <AST_NODE_PTR> node_queue;
 
-	STRING_LIST getProperties(AST_NODE_PTR_LIST, NODE_TYPE);
+	STRING_LIST getProperties(AST_NODE_PTR_LIST, NODE_TYPE, STRING_LIST);
+		/*
+		Description: Returns a STRING_LIST of the properties of the given node. Return empty list if no properties.
+		*/
 };
