f259fdef968b3a1ec01f93d08d7455ff80c01f29
Kyzure <Kyzurene@gmail.com>
2020-09-12 16:47:12 +0800

    Start unit test

---
+++ Team01/Code01/UnitTesting/TestPKB.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "PKB.h"
+#include "PKBBuilder.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting {
+	
+	TEST_CLASS(PKB) {
+	
+	public:
+		TEST_METHOD(getStatements) {
+			PKBBuilder pkb_builder();
+		}
+
+	};
+}
