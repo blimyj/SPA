3379bd9c19d08c0e42fb6b53116af0e2a66aca6b
WeomuCat <weomu.cat@protonmail.com>
2020-09-13 19:54:15 +0800

    Fixed followsT & parentT :)

--- Team01/Code01/source/PKB/Relationships/FollowsTable.cpp
+++ Team01/Code01/source/PKB/Relationships/FollowsTable.cpp
@@ -13,20 +13,21 @@ BOOLEAN FollowsTable::isFollows(STMT_NUM s1, STMT_NUM s2) {
 }
 
 BOOLEAN FollowsTable::isFollowsTransitive(STMT_NUM s1, STMT_NUM s2) {
-    // Check if Follows*(s1, s2) exists
-    if (follows_transitive_.count({ s1, s2 }) > 0) {
-        return true;
+    // Check if Follows*(s1, s2) exists, and return it's value
+    auto it1 = follows_transitive_.find({ s1, s2 });
+    if (it1 != follows_transitive_.end()) {
+        return it1->second;
     }
 
     // Check if s1 has a follower
-    auto it = statements_.find(s1);
-    if (it == statements_.end()) {
+    auto it2 = statements_.find(s1);
+    if (it2 == statements_.end()) {
         return false;
     }
 
     // Check if follower is transitive
     // Memoize transitive relationships :)
-    if (isFollowsTransitive(it->second, s2)) {
+    if (isFollowsTransitive(it2->second, s2)) {
         follows_transitive_.insert({ { s1, s2 }, true });
         return true;
     }
