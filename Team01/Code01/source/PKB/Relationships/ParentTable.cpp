f24c9454207eeb93bae7b5c9d4565ba5897aff51
WeomuCat <weomu.cat@protonmail.com>
2020-09-13 19:54:15 +0800

    Fixed followsT & parentT :)

--- Team01/Code01/source/PKB/Relationships/ParentTable.cpp
+++ Team01/Code01/source/PKB/Relationships/ParentTable.cpp
@@ -12,20 +12,21 @@ BOOLEAN ParentTable::isParent(STMT_NUM s1, STMT_NUM s2) {
 }
 
 BOOLEAN ParentTable::isParentTransitive(STMT_NUM s1, STMT_NUM s2) {
-    // Check if Parent*(s1, s2) exists
-    if (parent_transitive_.count({ s1, s2 }) > 0) {
-        return true;
+    // Check if Parent*(s1, s2) exists, and return it's value
+    auto it1 = parent_transitive_.find({ s1, s2 });
+    if (it1 != parent_transitive_.end()) {
+        return it1->second;
     }
 
     // Check if s1 is a container
-    auto it = statements_.find(s1);
-    if (it == statements_.end()) {
+    auto it2 = statements_.find(s1);
+    if (it2 == statements_.end()) {
         return false;
     }
 
     // Iterate children of s1 and check if they are transitive
     // Memoize transitive relationships :)
-    for (STMT_NUM c : it->second) {
+    for (STMT_NUM c : it2->second) {
         if (isParentTransitive(c, s2)) {
             parent_transitive_.insert({ { s1, s2 }, true });
             return true;
