21889d6dd040b7257f06859b40aa2bab150d43fc
Dan <janiceongyw@gmail.com>
2020-09-18 04:52:57 +0800

    Add TestParser file (currently not functional)

---
+++ Team01/Code01/UnitTesting/TestParser.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <typeinfo>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+#include "../source/Parser.h"
+#include "../source/ParserPrintTree.h"
+#include "../source/PKB.h"
