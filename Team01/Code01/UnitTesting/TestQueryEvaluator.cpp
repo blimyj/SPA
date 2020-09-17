a6d6947b337335a9e702fd166762a6a0d6eba6c4
limyuhui <yuhuilim1999@gmail.com>
2020-09-17 12:18:33 +0800

    Skeleton structure of actual evaluator

--- Team01/Code01/UnitTesting/TestQueryEvaluator.cpp
+++ Team01/Code01/UnitTesting/TestQueryEvaluator.cpp
@@ -4,6 +4,7 @@
 #include "../source/PKBStub.h"
 #include "../source/QueryNode.h"
 #include "../source/QueryNodeType.h"
+#include "../source/PKB/ASTNode/StatementNode.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -51,23 +52,46 @@ namespace UnitTesting
 				print e;
 			}
 			*/
+			// PKB
 			PKBBuilder builder;
 			builder.addVariableNode(std::make_shared<VariableNode>("a"));
 			builder.addVariableNode(std::make_shared<VariableNode>("b"));
 			builder.addVariableNode(std::make_shared<VariableNode>("c"));
 			builder.addVariableNode(std::make_shared<VariableNode>("d"));
 			builder.addVariableNode(std::make_shared<VariableNode>("e"));
-			builder.addStatementNode(std::make_shared<StatementNode>());
-			PKB pkb = builder.build();
 
+			StatementNode s1;
+			s1.setStatementNumber(1);
+			StatementNode s2;
+			s2.setStatementNumber(2);
+			StatementNode s3;
+			s3.setStatementNumber(3);
+			StatementNode s4;
+			s4.setStatementNumber(4);
+			StatementNode s5;
+			s5.setStatementNumber(5);
+			builder.addStatementNode(std::make_shared<StatementNode>(s1));
+			builder.addStatementNode(std::make_shared<StatementNode>(s2));
+			builder.addStatementNode(std::make_shared<StatementNode>(s3));
+			builder.addStatementNode(std::make_shared<StatementNode>(s4));
+			builder.addStatementNode(std::make_shared<StatementNode>(s5));
+
+			builder.addFollows(1, 2);
+			builder.addFollows(2, 3);
+			builder.addFollows(3, 4);
+			builder.addFollows(4, 5);
+			PKB pkb = builder.build();
+			
 			// Query: "stmt s1, s2; variable v; Select v such that Follows(s1,s2)"
 			// Get processed_synonyms and processed clauses
 			QueryNode variable_node = QueryNode();
 			variable_node.setSynonymNode({ QuerySynonymType::variable }, "v");
-			std::unordered_map<std::string, QueryNode> processed_synonyms = { {"v", variable_node} };
+			QueryNode s1_node = QueryNode();
+			variable_node.setSynonymNode({ QuerySynonymType::stmt }, "s1");
+			QueryNode s2_node = QueryNode();
+			variable_node.setSynonymNode({ QuerySynonymType::stmt }, "s2");
+			std::unordered_map<std::string, QueryNode> processed_synonyms = { {"v", variable_node}, {"s1", s1_node}, {"s2", s2_node} };
 
-
-			//"Select v such that Follows(3,s)"
 			QueryNode root = QueryNode();
 			root.setNodeType({ QueryNodeType::select });
 			QueryNode child1 = QueryNode();
@@ -77,9 +101,9 @@ namespace UnitTesting
 			QueryNode child_child1 = QueryNode();
 			child_child1.setNodeType({ QueryNodeType::follows});
 			QueryNode child_child_child1 = QueryNode();
-			child_child_child1.setSynonymNode({ QuerySynonymType::variable }, "s1");
+			child_child_child1.setSynonymNode({ QuerySynonymType::stmt }, "s1");
 			QueryNode child_child_child2 = QueryNode();
-			child_child_child2.setSynonymNode({ QuerySynonymType::variable }, "s2");
+			child_child_child2.setSynonymNode({ QuerySynonymType::stmt }, "s2");
 			QueryNode child_child1_children[] = { child_child_child1, child_child_child2 };
 			child_child1.setChildren(child_child1_children);
 			QueryNode child2_children[] = {child_child1};
@@ -89,10 +113,10 @@ namespace UnitTesting
 
 			QueryNode processed_clauses = root; //stores root node of the tree
 
-
+			// Evaluate
 			QueryEvaluator qe = QueryEvaluator(pkb);
 			QUERY_RESULT result = qe.evaluateQuery(processed_synonyms, processed_clauses);
-			QUERY_RESULT correct_result = "C++ god, Help, me";
+			QUERY_RESULT correct_result = "a, b, c, d, e";
 			Assert::IsTrue(result.compare(correct_result) == 0);
 		}
 	};
