954506e45edf9da824c032b5a2c315894171af28
limyuhui <yuhuilim1999@gmail.com>
2020-09-18 06:15:59 +0800

    Integrate QueryPreProcessor with QueryProcessor

--- Team01/Code01/source/QueryProcessor.cpp
+++ Team01/Code01/source/QueryProcessor.cpp
@@ -16,35 +16,21 @@ QueryProcessor::QueryProcessor(PKB pkb) {
 
 QUERY_RESULT QueryProcessor::processQuery(QUERY query) {
 
-	/* Uncomment when preprocessor is done
+	//Uncomment when preprocessor is done
 	
 	// QPP
-	const std::string empty_result = "";
 	QueryPreProcessor pre_processor = QueryPreProcessor();
-	PROCESSED_QUERY process_queries = pre_processor.parse(QUERY q);
-	PROCESSED_SYNONYMS processed_synonyms = pre_processor.preProcessSynonyms(QUERY q); //returns table of synonym nodes
-		if (processed_synonyms.empty()) { return empty_result; } // either no synonym or syntax error, both should return emptyresult
-	PROCESSED_CLAUSES processed_clauses = pre_processor.preProcessClauses(QUERY q); //returns query tree of clause nodes
-		 if (processed_queries == NULL) { return empty_result; } // null result means that there is syntax error, return empty result
+	SPLIT_QUERY splitted = pre_processor.splitQuery(query);
+	PROCESSED_SYNONYMS processed_synonyms = pre_processor.preProcessSynonyms(splitted[0]);
+	PROCESSED_CLAUSES processed_clauses = pre_processor.preProcessClauses(processed_synonyms, splitted[1]); //returns table of synonym nodes
 
-	// Wait for preprocessor
 	
-	const std::string empty_result = "";
-	QueryPreProcessor pre_processor = QueryPreProcessor();
-	SPLIT_QUERY split_query = pre_processor.splitQuery(query); // returns vector containing seperated declarations and clauses
-	PROCESSED_SYNONYMS processed_synonyms = pre_processor.preProcessSynonyms(split_query.at(0)); // returns table of synonym nodes
-		if (processed_synonyms.empty()) { return empty_result; } // either no synonym or syntax error, both should return emptyresult
-		PROCESSED_CLAUSES processed_clauses = pre_processor.preProcessClauses(processed_synonyms, split_query.at(1)); //returns query tree of clause nodes
-		if (processed_clauses.getNodeType() == QueryNodeType::unassigned) { return empty_result; } // null result means that there is syntax error, return empty result
-	
-
-
 	// QE
 	QueryEvaluator query_evaluator = QueryEvaluator(this->pkb);
 
 	QUERY_RESULT query_result = query_evaluator.evaluateQuery(processed_synonyms, processed_clauses);
 	return query_result;
-	*/
+	
 	return "";
 
 	/*
