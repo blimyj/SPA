3337a3a943e5d125b8bedc7835cd2d2061a30bc1
limyuhui <yuhuilim1999@gmail.com>
2020-09-20 05:43:18 +0800

    Add files for personal autotesting

--- Team01/Code01/Gui/GUIWrapper.cpp
+++ Team01/Code01/Gui/GUIWrapper.cpp
@@ -71,7 +71,7 @@ void GUIWrapper::evaluate(std::string query, std::list<std::string>& results){
 
 	// store the answers to the query in the results list (it is initially empty)
 	// each result must be a string.
-	for (auto query : query_result) {
+	for (std::string query : query_result) {
 		results.push_back(query);
 	}
 
