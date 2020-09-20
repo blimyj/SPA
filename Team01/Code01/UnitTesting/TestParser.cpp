fb34c80a226d19efa031be8d305896b2f39be183
Dan <janiceongyw@gmail.com>
2020-09-20 18:23:02 +0800

    Update TestParser

--- Team01/Code01/UnitTesting/TestParser.cpp
+++ Team01/Code01/UnitTesting/TestParser.cpp
@@ -2,15 +2,16 @@
 
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include "TestParser.h"
 
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cstdlib>
 
-#include "../source/Parser.h"
-#include "../source/PKB.h"
+#include "Parser.h"
+#include "PKB.h"
+
+typedef std::shared_ptr<PKB> PKB_PTR;
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -25,26 +26,35 @@ namespace UnitTesting {
 		}
 		*/
 
-		std::shared_ptr<PKB> pkb;
-		
+		PKB_PTR pkb;
+		PKB_PTR actual_pkb;
+		/*
 		TEST_METHOD_INITIALIZE(PKBInitialize) {
 			// PKB
 			PKBBuilder builder;
 
+			// Make ProgramNode
+			PROGRAM_NODE_PTR prog1 = std::make_shared<ProgramNode>();
+			builder.setProgramNode(prog1);
+
 			// Make StatementListNode
-			std::shared_ptr<StatementListNode> sl1 = std::make_shared<StatementListNode>();
+			STMT_LIST_NODE_PTR sl1 = std::make_shared<StatementListNode>();
+			builder.addStatementListNode(sl1);
 
 			// Make ProcedureNode Main
-			std::shared_ptr<ProcedureNode> prc1 = std::make_shared<ProcedureNode>();
+			PROC_NODE_PTR prc1 = std::make_shared<ProcedureNode>();
 			prc1->setProcedureName("main");
 			prc1->setProcedureStatementListNode(sl1);
+			builder.addProcedureNode(prc1);
+
+			prog1->addProcedureNode(prc1);
 			
 			// Make VariableNodes a, b, c
-			std::shared_ptr<VariableNode> v1 = std::make_shared<VariableNode>();
+			VAR_NODE_PTR v1 = std::make_shared<VariableNode>();
 			v1->setVariableName("a");
-			std::shared_ptr<VariableNode> v2 = std::make_shared<VariableNode>();
+			VAR_NODE_PTR v2 = std::make_shared<VariableNode>();
 			v2->setVariableName("b");
-			std::shared_ptr<VariableNode> v3 = std::make_shared<VariableNode>();
+			VAR_NODE_PTR v3 = std::make_shared<VariableNode>();
 			v3->setVariableName("c");
 
 			builder.addVariableNode(v1);
@@ -52,17 +62,17 @@ namespace UnitTesting {
 			builder.addVariableNode(v3);
 
 			// Make ConstNode 1
-			std::shared_ptr<ConstantNode> c1 = std::make_shared<ConstantNode>();
+			CONSTANT_NODE_PTR c1 = std::make_shared<ConstantNode>();
 			c1->setValue("1");
 
 			builder.addConstantNode(c1);
 
 			// Make ExpressionNode
-			std::shared_ptr<ExpressionNode> e1 = std::make_shared<ExpressionNode>();
+			EXPR_NODE_PTR e1 = std::make_shared<ExpressionNode>();
 			e1->setExpressionType(EXPR_TYPE::undefined);
 
 			// Make AssignNode
-			std::shared_ptr<AssignNode> a1 = std::make_shared<AssignNode>();
+			ASSIGN_NODE_PTR a1 = std::make_shared<AssignNode>();
 			a1->setStatementNumber(1);
 			a1->setVariableNode(v1);
 			a1->setExpressionNode(e1);
@@ -70,14 +80,14 @@ namespace UnitTesting {
 			builder.addAssignNode(a1);
 
 			// Make PrintNode
-			std::shared_ptr<PrintNode> p1 = std::make_shared<PrintNode>();
+			PRINT_NODE_PTR p1 = std::make_shared<PrintNode>();
 			p1->setStatementNumber(2);
 			p1->setVariableNode(v2);
 			
 			builder.addPrintNode(p1);
 			
 			// Make ReadNode
-			std::shared_ptr<ReadNode> r1 = std::make_shared<ReadNode>();
+			READ_NODE_PTR r1 = std::make_shared<ReadNode>();
 			r1->setStatementNumber(3);
 			r1->setVariableNode(v3);
 
@@ -90,9 +100,9 @@ namespace UnitTesting {
 			s2.setStatementNumber(2);
 			StatementNode s3;
 			s3.setStatementNumber(3);
-			std::shared_ptr<StatementNode> s1ptr = std::make_shared<StatementNode>(s1);
-			std::shared_ptr<StatementNode> s2ptr = std::make_shared<StatementNode>(s2);
-			std::shared_ptr<StatementNode> s3ptr = std::make_shared<StatementNode>(s3);
+			STMT_NODE_PTR s1ptr = std::make_shared<StatementNode>(s1);
+			STMT_NODE_PTR s2ptr = std::make_shared<StatementNode>(s2);
+			STMT_NODE_PTR s3ptr = std::make_shared<StatementNode>(s3);
 			builder.addStatementNode(s1ptr);
 			builder.addStatementNode(s2ptr);
 			builder.addStatementNode(s3ptr);
@@ -103,47 +113,44 @@ namespace UnitTesting {
 			sl1->addStatementNode(s3ptr);
 
 			pkb = std::make_shared<PKB>(builder.build());
+
+			Parser parser = Parser();
+			actual_pkb =
+				std::make_shared<PKB>(parser.parseFile("../UnitTesting/Parser/TestParser-1.txt"));
 		}
 		
-		TEST_METHOD(makeSimpleAst_checkProcedure_Valid_Success)
-		{
-			PKBInitialize();
-			Parser parser = Parser();
-			std::shared_ptr<PKB> actual_pkb =
-				std::make_shared<PKB>(parser.parseFile("../UnitTesting/Parser/TestParser-1.txt"));
-			
-			PROC_NAME_LIST expected_list = pkb->getProcedureNameList();
-			PROC_NAME_LIST actual_list = actual_pkb->getProcedureNameList();
+		TEST_METHOD(makeSimpleAst_makeProgram)
+		{	
+			PROGRAM_NODE_PTR expected_prog_node = pkb->getProgramNode();
+			PROGRAM_NODE_PTR actual_prog_node = actual_pkb->getProgramNode();
 
-			/*
-			std::string actual_string;
-			
-			for (std::vector<std::string>::const_iterator i = actual_list.begin(); i != actual_list.end(); ++i)
-				actual_string += *i + ' ';
-
-			Logger::WriteMessage(actual_string.c_str());
-			*/
-			Assert::IsTrue(std::equal(expected_list.begin(), expected_list.end(), actual_list.begin()));
-		}
-
-		/*
-		TEST_METHOD(testBasicProcedure)
-		{
-			ParserPrintTree printer;
-			Parser parser = Parser();
-			std::shared_ptr <PKB> pkb =
-				std::make_shared<PKB> (parser.parseFile("../UnitTesting/Parser/TestParser-1.txt"));
-			AST_NODE_PTR parent_node = pkb->getProcedures().at(0);
-
-			std::string actual = printer.parserPrintTree(parent_node);
-
-			std::ifstream infile{ "../UnitTesting/Parser/TestParser-1-out.txt" };
-			std::string expected{ std::istreambuf_iterator<char>(infile), std::istreambuf_iterator<char>() };
-			infile.close();
-
-			Assert::IsTrue(actual == expected);
-
+			Assert::IsTrue(typeid(*expected_prog_node) == typeid(*actual_prog_node));
 		}
 		*/
+		TEST_METHOD(makeSimpleAst_makeProcedure)
+		{
+			
+			/*
+			PROGRAM_NODE_PTR expected_prog_node = pkb->getProgramNode();
+			PROGRAM_NODE_PTR actual_prog_node = actual_pkb->getProgramNode();
+			*/
+			
+			// Make Trial ProgramNode'
+			PKBBuilder builder;
+
+			PROGRAM_NODE_PTR prog10 = std::make_shared<ProgramNode>();
+			builder.setProgramNode(prog10);
+			PROC_NODE_PTR proc10 = std::make_shared<ProcedureNode>();
+
+			prog10->addProcedureNode(proc10);
+			builder.addProcedureNode(proc10);
+			PKB test_pkb = builder.build();
+
+			PROGRAM_NODE_PTR test_prog = test_pkb.getProgramNode();
+			PROC_NODE_PTR_LIST test_proc_list = test_prog->getProcedureNodeList();
+
+			Assert::IsTrue(true);
+		}
+
 	};
 }
