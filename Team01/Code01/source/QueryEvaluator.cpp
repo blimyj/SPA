9a925a63884d9c87d54fd1d5b9a97620ec0db746
limyuhui <yuhuilim1999@gmail.com>
2020-09-23 01:23:12 +0800

    Remove unnecessary comments

--- Team01/Code01/source/QueryEvaluator.cpp
+++ Team01/Code01/source/QueryEvaluator.cpp
@@ -107,24 +107,6 @@ QUERY_RESULT QueryEvaluator::evaluateQuery(PROCESSED_SYNONYMS synonyms, PROCESSE
 			//else if (relationship_type == QueryNodeType::modifiesP) {}
 		}
 		else if (clause_type == QueryNodeType::pattern) {
-			// magic
-			// Only 1 node on the LHS and search for only 1 node on the RHS (Iteration 1)
-			// Depends on whether the pattern is searchign for a constant or a variable (eg x = 3 or x = v )
-			// Example pattern a(_, _"x"_) a(_, _"4"_) a("w", _"x"_) a("i", _"x"_) a("mango", _"x"_ ) a("mango", _"4"_)
-			
-			// idea for RHS variable:
-				//get assign nodes, get their stmt numbers, check isUses(stmtnum, RHS thing eg 3/v)
-				//if LHS is not wildcard, then check isModifies(stmtNum, v)
-			// for constants:
-				// find all the constant nodes, for each node, go up and find a stmt node (while if print read assign )
-				// if not assign:
-					//end
-				// else:
-					// for the assign node stmtNum, check isModifies(stmtNum, v)
-			
-			// RHS also has _ too :( yep then omg wait lemmme think
-			// ><
-
 			
 			// a (_, _)
 			QueryNode pattern = clause;
@@ -226,92 +208,6 @@ QUERY_RESULT QueryEvaluator::evaluateQuery(PROCESSED_SYNONYMS synonyms, PROCESSE
 			clause_bool = (clause_result_list.getNumRows() > 0);
 		}
 			
-			// Eg pattern a1("woof", _"x"_)
-			//clause_bool = false;
-			//ResultList clause_result_list;
-			//ResultList initial_assign_list;
-			//std::vector<ASSIGN_NODE_PTR> all_assigns = pkb.getAssigns();
-
-			//// QueryNodeContent -> a1
-			//QueryNode pattern_assign_synonym = clause.getChildren()[0];
-			//// add column head a1
-			//SYNONYM_NAME synonym_name = pattern_assign_synonym.getString();
-			//clause_result_list.addColumn(synonym_name);
-			//initial_assign_list.addColumn(synonym_name, pkb.getAssignNumList());
-
-			//// QueryNodeContent -> "woof"
-			//QueryNode lhs_node = clause.getChildren()[1];
-			//STRING lhs = lhs_node.getString();
-			//auto lhs_node_type = lhs_node.getNodeType();
-			
-			//// ASTNode -> "x"
-			//QueryNode rhs_node = clause.getChildren()[2];
-			//std::shared_ptr<ASTNode> rhs = (rhs_node.getAstNode());
-			//NODE_TYPE rhs_node_type = rhs.get()->getNodeType();
-			
-
-			///* Ways to cast ASTNode to more specific nodes (use for VariableNode and ConstantNode)
-			//AssignNode* assign = static_cast<AssignNode*>(rhs_node_content.get());
-			//auto a = assign->getExpressionNode();
-
-			//VariableNode* assign = static_cast<VariableNode*>(rhs_node_content.get());
-			//*/
-			
-			
-			//if (rhs_node_type == NodeTypeEnum::variableNode) {
-			//	if (lhs_node_type == QueryNodeType::synonym) { //ie lhs == QueryNodeType::ident || lhs == QueryNodeType::synonym
-			//		// pattern a(synonym/'IDENT', v)
-			//		// check isModifies(synonym/'IDENT', v) && isUses(synonym/'IDENT', v)
-			//		// if true:
-			//			// somehow find the stmtNum, add to clause_result_list
-					
-			//	}
-			//	else if (lhs_node_type == QueryNodeType::ident) { // ok!!oo I hope my logic makes sense >< ohh i havent read yet i was writing mine in the if else AHAHAHA okk lemme read
-			//		// get VarNodeTable
-			//		// find IDENT variable in VarNodeTable
-			//		// with the VarNode, getParent() to get the direct parent which is an AssignNode
-			//		// AssignNode get the stmtNum
-			//		// with the stmtNum check isModifies(stmtNum, v) && isUses(stmtNum, v)
-
-			//	}
-			//	else if (lhs_node_type == QueryNodeType::wild_card) {
-			//		// pattern a(_, _"v"_ )
-			//		// for all stmt in stmts:
-			//			// check isUses(stmt, v)
-
-			//	}
-			//	else {
-			//		throw "QE Pattern lhs argument is not synonym, IDENT, wild card -- INVALID";
-			//	}
-			//}
-			//else if (rhs_node_type == NodeTypeEnum::constantNode) {
-			//	// search up until we hit assignNode
-			//	// get AssignNode stmtNum 
-			//	// ohh you don't want to traverse the AST? I see! ohh i thought it would be quite hard to do that maybe when im more familiar with ASTNode then i try? but now im manually checking
-			//	// ohh this one looks scary it's like traversing the AST ohh>< LOL YEAH yeahhhh maybe can practice traversing heree im worried AHAHAHAHA
-			//	// i can try writing some code to traverse the assign node! ohhh okay! 
-			//}
-			//else if (rhs_node.getNodeType() == QueryNodeType::wild_card) {
-			//	if (lhs_node_type == QueryNodeType::wild_card) {
-			//		// pattern a(_, _)
-			//		// return all assign statements
-
-			//	}
-			//	else if (lhs_node_type == QueryNodeType::synonym) {
-			//		// pattern a(v, _)
-
-			//	}
-			//	else if (lhs_node_type == QueryNodeType::ident) {
-			//		// pattern a("mango", _)
-
-			//	}
-			//	else {
-			//		throw "QE Pattern lhs argument is not synonym, IDENT, wild card -- INVALID";
-			//	}
-			//}
-			//else {
-			//	throw "QE Pattern rhs argument is not variable or constant --  INVALID";
-			//}
 
 		// if the clause_bool is true => merge result_list with clause_result_list
 		// if the clause_bool is false => return ""
