8c3c216ef29c018b941647468ebf9792f45b77ec
blimyj21@gmail.com <blimyj21@gmail.com>
2020-09-23 23:06:34 +0800

    Change parse functions to use abstract data types instead

--- Team01/Code01/source/Parser.h
+++ Team01/Code01/source/Parser.h
@@ -10,59 +10,52 @@
 
 typedef std::string STRING;
 typedef std::vector<std::string> LIST_OF_STRINGS;
-typedef std::deque<STRING>* STMT_TOKEN_QUEUE;
-typedef std::deque<STRING>* PROCESS_TOKEN_QUEUE;
+typedef std::deque<STRING>* STMT_TOKEN_QUEUE_PTR;
+typedef std::deque<STRING>* PROCESS_TOKEN_QUEUE_PTR;
+typedef std::deque<STRING> STMT_TOKEN_QUEUE;
+typedef std::deque<STRING> PROCESS_TOKEN_QUEUE;
 typedef int STMT_NUMBER;
+typedef bool BOOLEAN_TYPE;
+typedef int PRECEDENCE;
 
 class Parser {
 public:
-	int Parse();
 	PKB parseFile(STRING str);
-	int parseText(LIST_OF_STRINGS l_of_str);
-private:
-	int parseProcedure();
-	int parseRead();
-	int parsePrint();
-	int parseAssign();
 
-	int parseWhile();
-	int parseIfThen();
-	int parseElse();
+private:
+	void parseProcedure();
+	void parseRead();
+	void parsePrint();
+	void parseAssign();
+
+	void parseWhile();
+	void parseIfThen();
+	void parseElse();
 	//int parseCall(STRING str);
-	int parseStmtListClose();
+	void parseStmtListClose();
 
 	STRING getNextToken(std::istreambuf_iterator<char>* iter, std::istreambuf_iterator<char> eos);
 
-	std::shared_ptr<ProgramNode> program_node_;
-	std::shared_ptr<ASTNode> current_parent_node_;
+	PROGRAM_NODE_PTR program_node_;
+	AST_NODE_PTR current_parent_node_;
 	PKBBuilder pkb_builder_;
 
-	std::deque<STRING> stmt_token_queue_;
-	std::deque<STRING> process_token_stream_;
+	STMT_TOKEN_QUEUE stmt_token_queue_;
+	PROCESS_TOKEN_QUEUE process_token_stream_;
 
 	STMT_NUMBER stmt_num_;
 
 	//Helper variables & functions for parsing expressions
-	int takesPrecedent(OperatorTypeEnum l_op, OperatorTypeEnum r_op);
+	PRECEDENCE takesPrecedent(OperatorTypeEnum l_op, OperatorTypeEnum r_op);
 	ExpressionTypeEnum getExpressionType(OperatorTypeEnum op);
 
-	int takesPrecedent(RelOperatorTypeEnum l_op, RelOperatorTypeEnum r_op);
+	PRECEDENCE takesPrecedent(RelOperatorTypeEnum l_op, RelOperatorTypeEnum r_op);
 	ExpressionTypeEnum getExpressionType(RelOperatorTypeEnum op);
 	RelationTypeEnum getArithmeticRelationType(RelOperatorTypeEnum op);
 	ConditionTypeEnum getBooleanRelationType(RelOperatorTypeEnum op);
 
-	bool isArithmeticOp(RelOperatorTypeEnum op);
-	bool isRelationOp(RelOperatorTypeEnum op);
-	bool isBooleanOp(RelOperatorTypeEnum op);
-
-	//Debugger Functions to print out AST
-	int printTree(AST_NODE_PTR);
-		/*
-		Description: Prints the nodes in the AST out, given the root node.
-		*/
-	LIST_OF_STRINGS getProperties(AST_NODE_PTR, NODE_TYPE);
-		/*
-		Description: Returns a STRING_LIST of the properties of the given node. Return empty list if no properties.
-		*/
+	BOOLEAN_TYPE isArithmeticOp(RelOperatorTypeEnum op);
+	BOOLEAN_TYPE isRelationOp(RelOperatorTypeEnum op);
+	BOOLEAN_TYPE isBooleanOp(RelOperatorTypeEnum op);
 };
 
