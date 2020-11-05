09130328f4515a939bcd466d3dd45a126c3cb854
Kyzure <Kyzurene@gmail.com>
2020-11-06 01:51:36 +0800

    Fixed more queries

--- Team01/Code01/source/QueryPreProcessor.cpp
+++ Team01/Code01/source/QueryPreProcessor.cpp
@@ -807,7 +807,7 @@ PROCESSED_CLAUSES QueryPreProcessor::preProcessClauses(PROCESSED_SYNONYMS proc_s
 	bool is_bool = false;
 
 	CLAUSE_LIST all_clauses;
-	ClauseQueue clause_queue(false);
+	ClauseQueue clause_queue(true);
 
 	if (QueryValidator::isValidClause(c)) {
 		select_node.setNodeType({ QueryNodeType::select });
