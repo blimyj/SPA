eabf823448eae38ddb3299d3a3c9217fd7966ebc
blimyj21@gmail.com <blimyj21@gmail.com>
2020-11-07 16:45:06 +0800

    Change formatting for operator type enum header files

--- Team01/Code01/source/Parser/OperatorTypeEnum.h
+++ Team01/Code01/source/Parser/OperatorTypeEnum.h
@@ -1,5 +1,14 @@
 #pragma once
 enum class OperatorTypeEnum { 
-	opPlus, opMin, opMult, opDiv, opMod, opLparen, opRparen
-	, opAnd, opBoolNone, opNot, opOr
+	opPlus,
+	opMin,
+	opMult,
+	opDiv,
+	opMod,
+	opLparen,
+	opRparen,
+	opAnd,
+	opBoolNone,
+	opNot,
+	opOr
 };
